5aeac9eeb666c952437008940367563200902565
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-03-13 23:31:05 +0100

    Add floating action button to notes page

--- lib/notes.dart
+++ lib/notes.dart
@@ -13,8 +13,37 @@ class NotesPage extends StatelessWidget {
         title: const Text('Notatki'),
       ),
       drawer: const CustomDrawer(),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: AddNoteButton(
+        onPressed: () {},
+      ),
       body: const Center(
         child: Text('Notatki'),
+      ),
+    );
+  }
+}
+
+class AddNoteButton extends StatelessWidget {
+  const AddNoteButton({
+    required this.onPressed,
+    super.key,
+  });
+
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: FloatingActionButton(
+        onPressed: onPressed,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: const Icon(Icons.add),
       ),
     );
   }
