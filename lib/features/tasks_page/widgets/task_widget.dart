cf98dbab3eb083f291703b15d3b070197dc05716
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-03-19 13:35:04 +0100

    Improve task widget by expanadable elements

--- lib/features/tasks_page/widgets/task_widget.dart
+++ lib/features/tasks_page/widgets/task_widget.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
-class TaskWidget extends StatelessWidget {
+class TaskWidget extends StatefulWidget {
   const TaskWidget({
     super.key,
   });
 
   @override
+  State<TaskWidget> createState() => _TaskWidgetState();
+}
+
+class _TaskWidgetState extends State<TaskWidget> {
+  bool isTaskDescriptionTapped = false;
+  bool isTaskTitleTapped = false;
+  int taskDescriptionMaxLines = 3;
+  int taskTitleMaxLines = 1;
+  @override
   Widget build(BuildContext context) {
-    return const TaskWidgetBody(
+    return TaskWidgetBody(
       taskId: '',
       taskTitle: 'Przykładowy tytuł taska',
       taskEditDate: '10.03.2023',
@@ -15,6 +24,38 @@ class TaskWidget extends StatelessWidget {
       taskPriority: 'Niski',
       taskDescription:
           'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.',
+      taskDescriptionMaxLines: taskDescriptionMaxLines,
+      taskTitleMaxLines: taskTitleMaxLines,
+      onTaskDescriptionTap: () {
+        setState(() {
+          if (isTaskDescriptionTapped == false) {
+            isTaskDescriptionTapped = true;
+            isTaskTitleTapped = true;
+            taskDescriptionMaxLines = 15;
+            taskTitleMaxLines = 3;
+          } else {
+            isTaskDescriptionTapped = false;
+            isTaskTitleTapped = false;
+            taskDescriptionMaxLines = 3;
+            taskTitleMaxLines = 1;
+          }
+        });
+      },
+      onTaskTitleTap: () {
+        setState(() {
+          if (isTaskTitleTapped == false) {
+            isTaskTitleTapped = true;
+            isTaskDescriptionTapped = true;
+            taskTitleMaxLines = 3;
+            taskDescriptionMaxLines = 15;
+          } else {
+            isTaskTitleTapped = false;
+            isTaskDescriptionTapped = false;
+            taskTitleMaxLines = 1;
+            taskDescriptionMaxLines = 3;
+          }
+        });
+      },
     );
   }
 }
@@ -27,6 +68,10 @@ class TaskWidgetBody extends StatelessWidget {
     required this.taskDeadlineDate,
     required this.taskPriority,
     required this.taskDescription,
+    required this.taskDescriptionMaxLines,
+    required this.taskTitleMaxLines,
+    required this.onTaskDescriptionTap,
+    required this.onTaskTitleTap,
     super.key,
   });
 
@@ -36,6 +81,10 @@ class TaskWidgetBody extends StatelessWidget {
   final String taskDeadlineDate;
   final String taskPriority;
   final String taskDescription;
+  final int taskDescriptionMaxLines;
+  final int taskTitleMaxLines;
+  final Function() onTaskDescriptionTap;
+  final Function() onTaskTitleTap;
 
   @override
   Widget build(BuildContext context) {
@@ -88,39 +137,42 @@ class TaskWidgetBody extends StatelessWidget {
           ),
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
+              GestureDetector(
+                onTap: onTaskTitleTap,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [
+                        Color.fromARGB(255, 13, 71, 161),
+                        Color.fromARGB(255, 84, 152, 255),
+                      ],
+                    ),
                   ),
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    colors: [
-                      Color.fromARGB(255, 13, 71, 161),
-                      Color.fromARGB(255, 84, 152, 255),
-                    ],
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5, bottom: 5),
-                      child: Text(
-                        taskTitle,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5, bottom: 5),
+                        child: Text(
+                          taskTitle,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: taskTitleMaxLines,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               Container(
@@ -168,13 +220,21 @@ class TaskWidgetBody extends StatelessWidget {
                       ],
                     ),
                     const Divider(thickness: 1.0),
-                    const Text('Opis zadania:'),
-                    const Padding(padding: EdgeInsets.only(top: 10)),
-                    Text(
-                      taskDescription,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 3,
-                      textAlign: TextAlign.left,
+                    GestureDetector(
+                      onTap: onTaskDescriptionTap,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Opis zadania:'),
+                          const Padding(padding: EdgeInsets.only(top: 10)),
+                          Text(
+                            taskDescription,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: taskDescriptionMaxLines,
+                            textAlign: TextAlign.left,
+                          ),
+                        ],
+                      ),
                     ),
                     const Divider(thickness: 1.0),
                     TextButton(onPressed: () {}, child: const Text('Edytuj'))
