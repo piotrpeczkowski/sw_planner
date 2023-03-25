3ab60cfd44397a94d8b9bc31fd3d101216f07880
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-03-25 19:28:59 +0100

    Initialize firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sw_planner/root/root.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
